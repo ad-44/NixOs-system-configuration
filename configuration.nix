57107080fcae842bb5afa55456f8797418687471
ad-44 <antoine.debille@gmail.com>
2025-10-15 14:17:16 +0200

    Added ly display manager

--- configuration.nix
+++ configuration.nix
@@ -81,10 +81,12 @@
   helix
   python313
   python313Packages.ipython
+  ly
   ];
   programs.niri.enable = true;
   programs.nh.enable = true;
   programs.git.enable = true;
+  services.displayManager.ly.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
